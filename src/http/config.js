a8a0f5e67ece4b5a5c670e67a8382323d6a96a67
MMMMintM <1962849542@qq.com>
2022-07-31 13:55:01 +0800

    commit

--- src/http/config.js
+++ src/http/config.js
@@ -1,5 +1,5 @@
 export default {
-    baseUrl: 'http://123.57.42.220:8090',
+    baseUrl: 'http://localhost:8090',
     timeOut: 5000,
     headers:{
         'Content-Type': 'application/json;charset=UTF-8'
