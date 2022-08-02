89a80e884e2468e6fefcd5349a6a1e026da0a17b
“HaiyiHuang” <1257504439@qq.com>
2022-08-02 16:32:40 +0800

    1.创建了Login组件，并实现了login()方法，能够返回token 2.创建了main组件，作为MainContent的默认显示组件 3.修改了Index组件,用router-view代替了MainContent，实现路由优化 4.修改了router路由表，采用子路由嵌套，实现路由优化 5.填充了store中的内容，因为侧边栏固定，所有里面的内容没用到 6.填充了api 7.在login.js中封装了login()方法

---
+++ src/views/Login.vue
@@ -0,0 +1,41 @@
+<template>
+  <label htmlFor="username">用户名：</label><input type="text" id="username" v-model="loginForm.username">
+  <label htmlFor="password">密码：</label><input type="text" id="password" v-model="loginForm.password">
+  <button @click="login">登录</button>
+</template>
+
+<script>
+
+export default {
+  name: "LoginPage",
+  data() {
+    return {
+      loginForm: {
+        username: 'liwei',
+        password: 'lw1234'
+      }
+    }
+  },
+  methods: {
+    login() {
+      // 1、拿到username和password
+      // 2、使用axios发送登录请求给后端（SpringBoot）
+
+      this.$api.login.login("/login", this.loginForm)
+          .then(res => {
+            // vuex（状态管理）
+            sessionStorage.setItem("token", res.token);
+            console.log(res);
+            this.$router.push("/index");
+          }).catch(err => {
+        console.log(err);
+        this.$router.push("/");//跳到登录页面
+      })
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
