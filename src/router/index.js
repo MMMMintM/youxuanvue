759d3628e898f3b6788b17319c28187902525186
MMMMintM <1962849542@qq.com>
2022-08-04 19:57:06 +0800

    1、修改路由

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Login from "@/views/Login";
 import MainContent from "@/views/MainContent";
 import {createRouter, createWebHistory} from "vue-router";
 import Test from "@/views/Test";
+import Main from "@/views/Main";
 
 const Index = () => import('../views/Index')
 /*
@@ -31,6 +32,14 @@ const routes = [
                 component: MainContent,
                 redirect: {path: '/index/mainContent/main'},
                 children: [
+                    {
+                        path: 'main',
+                        name: 'Main',
+                        meta: {
+                            name: 'main'
+                        },
+                        component: Main
+                    },
                     {
                         path: 'loopimg',
                         name:'Loopimg',
